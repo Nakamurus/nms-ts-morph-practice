df61fc6fab60a485e669d81c4bd767ed7499ea4b
nakamurus <3872mn@gmail.com>
2025-11-09 16:06:04 +0900

    get hooks

--- ast-practice.ts
+++ ast-practice.ts
@@ -1,5 +1,19 @@
 import ts from "ts-morph";
 
+// Extend SourceFile with custom getArrowFunctions method
+declare module "ts-morph" {
+  interface SourceFile {
+    getArrowFunctions(): ts.VariableDeclaration[];
+  }
+}
+
+ts.SourceFile.prototype.getArrowFunctions = function (this: ts.SourceFile) {
+  return this.getVariableDeclarations().filter((vd) => {
+    const initializer = vd.getInitializer();
+    return initializer && ts.Node.isArrowFunction(initializer);
+  });
+};
+
 // Initialize a new ts-morph project
 const project = new ts.Project({
   tsConfigFilePath: "tsconfig.app.json",
@@ -7,4 +21,16 @@ const project = new ts.Project({
 
 const sourceFiles = project.getSourceFiles();
 
-console.log("sourceFiles", sourceFiles);
+console.log("sourceFiles", sourceFiles.map((sf) => sf.getBaseName()));
+
+// get hooks of React components
+const hooks = sourceFiles.flatMap((sf) => {
+  const regularFunctions = sf.getFunctions();
+  const arrowFunctions = sf.getArrowFunctions();
+  const hooks = [...regularFunctions, ...arrowFunctions].filter((fn) =>
+    fn.getName()?.startsWith("use")
+  );
+  return hooks;
+});
+
+console.log("hooks", hooks.map((hook) => hook.getName()));
